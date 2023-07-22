bcace919757307a5f338469bb1dd1a8721859d18
Lucas Fernandes Aguiar <aguiarl@aljazeera.net>
2023-07-21 21:18:46 -0300

    Remove previouslsy rented movies from recommendations

--- app/services/recommendation_engine.rb
+++ app/services/recommendation_engine.rb
@@ -4,10 +4,11 @@ class RecommendationEngine
   end
 
   def recommendations
+    rented = User.find(@user_id).rented
     favorite_movies = User.find(@user_id).favorites
     movie_titles = favorite_movies.pluck(:title)
     genres = Movie.where(title: movie_titles).pluck(:genre)
     common_genres = genres.group_by{ |e| e }.sort_by{ |k, v| -v.length }.map(&:first).take(3)
-    Movie.where(genre: common_genres).order(rating: :desc).limit(10)
+    Movie.where(genre: common_genres).where.not(id: rented).order(rating: :desc).limit(10)
   end
 end
