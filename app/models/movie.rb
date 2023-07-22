f171bba4a8d55f833209fa681a95465d05e7ee21
Lucas Fernandes Aguiar <aguiarl@aljazeera.net>
2023-07-22 19:28:51 -0300

    Improved recommendation logic.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   has_many :favorite_movies
   has_many :users, through: :favorite_movies
+  scope :by_genre, ->(genre) { where(genre: genre) }
 end
   
--- app/services/recommendation_engine.rb
+++ app/services/recommendation_engine.rb
@@ -4,11 +4,10 @@ class RecommendationEngine
   end
 
   def recommendations
-    rented = User.find(@user_id).rented
-    favorite_movies = User.find(@user_id).favorites
-    movie_titles = favorite_movies.pluck(:title)
-    genres = Movie.where(title: movie_titles).pluck(:genre)
+    user = User.find(@user_id)
+    rented = user.rented
+    genres = Movie.where(id: user.favorites).pluck(:genre)
     common_genres = genres.group_by{ |e| e }.sort_by{ |k, v| -v.length }.map(&:first).take(3)
-    Movie.where(genre: common_genres).where.not(id: rented).order(rating: :desc).limit(10)
+    Movie.by_genre(common_genres).where.not(id: rented).order(rating: :desc).limit(10)
   end
 end
